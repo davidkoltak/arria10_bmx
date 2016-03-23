a7fb165d284beac679fdd7913684e0a6935eb345
David Koltak <davidkoltak@gmail.com>
2016-03-23 01:05:53 -0500

    Ready to try synth.

--- src/soc_a10_devkit_reva_emac_swif/emac_swif_avmm_adapter.v
+++ src/soc_a10_devkit_reva_emac_swif/emac_swif_avmm_adapter.v
@@ -1,30 +1,41 @@
 /*
-  A module to control EMAC Switch Interface transations from an Avalon MM 32-bit master
+  A module to control EMAC Switch Interface packet transactions 
+  from an Avalon MM 32-bit master.
   
   by David M. Koltak  03/22/2016
   
   Address     :  Register
 --------------------------------------------------------
+
+ --- TX INTERFACE ---
+     
         00000 : Tx CSR
-                   [0] - Busy
-                   [1] - Error
+                   [0] - Active
+                   [1] - Done
                 [11:2] - Last Tx Word
                [13:12] - Last Tx BE
                [28:14] - (reserved)
                   [29] - tx_chksum
                   [30] - tx_dispad
-                  [31] - tx_discrc  
-                                
-  00004-00FFF : Tx Buffer
-  
+                  [31] - tx_discrc 
+                   
+        00004 : Tx Status
+                
+  00008-00FFF : Tx Buffer
+     
+ --- RX INTERFACE ---
+     
         01000 : Rx CSR
-                   [0] - Valid
-                   [1] - Error
+                   [0] - Active
+                   [1] - Done
                 [11:2] - Last Rx Word
                [13:12] - Last Rx BE
-               [31:14] - (reserved)
-               
-  01004-01FFF : Rx Buffer
+               [30:14] - (reserved)
+                  [31] - Flush
+        
+        01004 : Rx Status
+        
+  01008-01FFF : Rx Buffer
   
 */
       
@@ -74,8 +85,6 @@ module emac_swif_avmm_adapter
   output switch_ari_frameflush,
   input switch_ari_timestamp_val
 );
-
-  assign lw_h2f_waitrequest = 1'b0;
   
   //
   // Transmit buffer
@@ -95,6 +104,7 @@ module emac_swif_avmm_adapter
   //
   
   reg tx_active;
+  reg tx_done;
   reg tx_sof;
   reg tx_eof;
   reg [9:0] tx_last;
@@ -109,6 +119,7 @@ module emac_swif_avmm_adapter
     if (rst)
     begin
       tx_active <= 1'b0;
+      tx_done <= 1'b0;
       tx_sof <= 1'b0;
       tx_eof <= 1'b0;
       tx_last <= 10'd0;
@@ -119,9 +130,10 @@ module emac_swif_avmm_adapter
       tx_word <= 10'd0;
       tx_data <= 32'd0;
     end
-    else if (lw_h2f_write && (lw_h2f_address == 20'd0) && (lw_h2f_byteenable == 4'b1111))
+    else if (lw_h2f_write && (lw_h2f_address == 20'h00000) && (lw_h2f_byteenable == 4'b1111))
     begin
-      tx_active <= 1'b1;
+      tx_active <= lw_h2f_writedata[0];
+      tx_done <= 1'b0;
       tx_sof <= 1'b1;
       tx_eof <= 1'b0;
       tx_last <= lw_h2f_writedata[11:2];
@@ -129,12 +141,13 @@ module emac_swif_avmm_adapter
       tx_discrc <= lw_h2f_writedata[31];
       tx_dispad <= lw_h2f_writedata[30];
       tx_chksum <= lw_h2f_writedata[29];
-      tx_word <= 10'd1;
-      tx_data <= tx_buf[10'd1];
+      tx_word <= 10'd3;
+      tx_data <= tx_buf[10'd2];
     end
     else if (tx_active && switch_ati_rdy)
     begin
       tx_active <= (tx_word != tx_last);
+      tx_done <= (tx_word == tx_last);
       tx_sof <= 1'b0;
       tx_eof <= (tx_word == tx_last);
       tx_word <= lw_h2f_writedata[9:0] + 10'd1;
@@ -157,8 +170,13 @@ module emac_swif_avmm_adapter
   assign switch_ati_chksum_ctrl = tx_chksum;
   
   reg [31:0] tx_status;
-  assign switch_ati_ack = 1'b1;
+  reg switch_ati_txstatus_val_d1;
   
+  assign switch_ati_ack = switch_ati_txstatus_val_d1;
+  
+  always @ (posedge clk)
+    switch_ati_txstatus_val_d1 <= switch_ati_txstatus_val;
+
   always @ (posedge clk)
     if (switch_ati_txstatus_val)
       tx_status = {14'd0, switch_ati_txstatus};
@@ -171,19 +189,44 @@ module emac_swif_avmm_adapter
   //
   
   reg rx_active;
-  reg rx_valid;
+  reg rx_capture;
+  reg rx_done;
+  reg [9:0] rx_last;
+  reg [1:0] rx_last_be;
   reg [9:0] rx_word;
+  reg rx_flush;
   
   always (posedge clk or posedge rst)
     if (rst)
     begin
       rx_active <= 1'b0;
-      rx_valid <= 1'b0;
-      rx_word <= 10'd0;
+      rx_capture <= 1'b0;
+      rx_done <= 1'b0;
+      rx_last <= 10'd0;
+      rx_last_be <= 2'b00;
+      rx_word <= 10'd2;
+      rx_flush <= 1'b0;
     end
-    else if (!rx_active && !rx_valid)
+    else if (lw_h2f_write && (lw_h2f_address == 20'h01000) && (lw_h2f_byteenable == 4'b1111))
     begin
-    
+      rx_active <= lw_h2f_writedata[0];
+      rx_capture <= 1'b0;
+      rx_done <= 1'b0;
+      rx_word <= 10'd2;
+      rx_flush <= lw_h2f_writedata[31];
+    end
+    else if (!rx_capture)
+    begin
+      rx_capture <= switch_ari_val && switch_ari_sof && !rx_done && rx_active;
+    end
+    else if (switch_ari_val)
+    begin
+      rx_active <= !switch_ari_eof;
+      rx_capture <= !switch_ari_eof;
+      rx_done <= switch_ari_eof;
+      rx_last <= rx_word;
+      rx_last_be <= switch_ari_be;
+      rx_word <= rx_word + 10'd1;
     end
   
   //
@@ -193,30 +236,61 @@ module emac_swif_avmm_adapter
   reg [31:0] rx_buf[1023:0];
   
   always @ (posedge clk or posedge rst)
-    if (rx_active && switch_ari_val)
+    if (rx_capture && switch_ari_val)
       rx_buf[rx_word] <= switch_ari_data;
   
   //
   // Receive
   //
-  
-  assign fifo_rx_in_valid = switch_ari_val;
-
-  assign fifo_rx_in_data = switch_ari_data;
-  assign fifo_rx_in_empty = ~switch_ari_be;
-  
-  assign fifo_rx_in_startofpacket = switch_ari_sof;
-  assign fifo_rx_in_endofpacket = switch_ari_eof;
    
   reg [31:0] rx_status;
-  assign switch_ari_ack = switch_ari_rxstatus_val || rx_active;
+  reg switch_ari_rxstatus_val_d1;
   
+  assign switch_ari_ack = switch_ari_rxstatus_val_d1 || rx_active;
+  
+  always @ (posedge clk)
+    switch_ari_rxstatus_val_d1 <= switch_ari_rxstatus_val;
+    
   always @ (posedge clk)
     if (switch_ari_rxstatus_val)
       rx_status = switch_ari_data;
   
+  assign switch_ari_frameflush = rx_flush;
+  
   assign switch_ari_pbl = 9'd0; // NOTE: 'switch_ari_rx_watermark' is unused
-  assign switch_ari_frameflush = 1'b0;
   // NOTE: 'switch_ari_timestamp_val' is unused
-   
+  
+  //
+  // Read from CSRs or buffers
+  //
+  
+  reg read_data_valid;
+  reg [31:0] read_data;
+  
+  always @ (posedge clk or posedge rst)
+    if (rst)
+      read_data_valid <= 1'b0;
+    else
+      read_data_valid <= lw_h2f_read;
+  
+  always @ (posedge clk)
+    if (lw_h2f_address[19:0] == 20'h00000)
+      read_data <= {tx_discrc, tx_dispad, tx_chksum, 15'd0, tx_last_be[1:0], tx_last[9:0], tx_done, tx_active};
+    else if (lw_h2f_address[19:0] == 20'h00004)
+      read_data <= tx_status;
+    else if (lw_h2f_address[19:0] == 20'h01000)
+      read_data <= {18'd0, rx_last_be[1:0], rx_last[9:0], rx_done, rx_active};
+    else if (lw_h2f_address[19:0] == 20'h01004)
+      read_data <= rx_status;
+    else if (lw_h2f_address[19:12] == 8'h00)
+      read_data <= tx_buf[lw_h2f_address[11:2]];
+    else if (lw_h2f_address[19:12] == 8'h01)
+      read_data <= rx_buf[lw_h2f_address[11:2]];
+    else
+      read_data <= 32'hFFFFFBAD;
+
+  assign lw_h2f_waitrequest = 1'b0;
+  assign lw_h2f_readdatavalid = read_data_valid;
+  assign lw_h2f_readdata = read_data;
+  
 endmodule
